31adc6c52fdba4e051cfd66939aa1377c2f11ac4
Nidhish Srivastava <nidhish0430@gmail.com>
2023-12-26 23:06:42 +0530

    .

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -3,6 +3,15 @@ import { NextResponse } from "next/server";
 
 export async function POST(request){
     const {projectName,investmentAmount,phoneNumber,email} = await request.json()
+    const mailData = {
+        from : "kunalriggroup@gmail.com",
+            to : email,
+            subject : "Investment Interest Received Successfully",
+            html : `<h2>We have successfully received your investment interest in our ${projectName} project for an amount of INR ${investmentAmount}</h2>
+            <p>We will contact you shortly. Get in contact with this project lead  ${phoneNumber}</p>
+            `
+    }
+    
     try {
         // const response = await transporter.sendMail({
         //     from : "kunalriggroup@gmail.com",
@@ -12,27 +21,6 @@ export async function POST(request){
         //     <p>We will contact you shortly. Get in contact with this project lead  ${phoneNumber}</p>
         //     `
         // })
-        await new Promise((resolve, reject) => {
-            // verify connection configuration
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log("Server is ready to take our messages");
-                    resolve(success);
-                }
-            });
-        });
-        const mailData = {
-            from : "kunalriggroup@gmail.com",
-                to : email,
-                subject : "Investment Interest Received Successfully",
-                html : `<h2>We have successfully received your investment interest in our ${projectName} project for an amount of INR ${investmentAmount}</h2>
-                <p>We will contact you shortly. Get in contact with this project lead  ${phoneNumber}</p>
-                `
-        }
-        
         const response = await new Promise((resolve,reject)=>{
             transporter.sendMail(mailData,(err,info)=>{
                 if(err){
@@ -45,7 +33,6 @@ export async function POST(request){
                 }
             })
         })
-        console.log(response);
         return NextResponse.json(response)
     } catch (error) {
         
