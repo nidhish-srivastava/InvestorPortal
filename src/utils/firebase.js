dd6c6c0969c1db62367f04198351eb00b53e5686
Nidhish Srivastava <nidhish0430@gmail.com>
2024-03-22 00:09:03 +0530

    .

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,15 +4,14 @@ import {getAuth} from "firebase/auth"
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APPID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-};
-
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENT_ID
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
