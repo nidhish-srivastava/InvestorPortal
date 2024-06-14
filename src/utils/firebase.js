354763949c89f2b7d665610b2db396979996405d
Nidhish Srivastava <nidhish0430@gmail.com>
2024-06-15 01:09:18 +0530

    fix sec.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,13 +4,6 @@ import {getAuth} from "firebase/auth"
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyAwvFhm9Mk4iAmAaQYkduljqVnGp-d2evc",
-  // authDomain: "investorpanel-2fb0d.firebaseapp.com",
-  // projectId: "investorpanel-2fb0d",
-  // storageBucket: "investorpanel-2fb0d.appspot.com",
-  // messagingSenderId: 719953933024,
-  // appId: "1:719953933024:web:4c903302f108a332fbd685",
-  // measurementId: "G-B69D9JZJNN"
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
