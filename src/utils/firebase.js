cb19fa7920314858ca1126dfef58830b4e4415eb
Nidhish Srivastava <nidhish0430@gmail.com>
2024-06-15 00:52:02 +0530

    updated env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -11,13 +11,14 @@ const firebaseConfig = {
   // messagingSenderId: 719953933024,
   // appId: "1:719953933024:web:4c903302f108a332fbd685",
   // measurementId: "G-B69D9JZJNN"
-  apiKey: "AIzaSyDZera0RgnI-l18NNdA7Rl3ZV__B1uM1Ts",
-  authDomain: "investorportal-bad23.firebaseapp.com",
-  projectId: "investorportal-bad23",
-  storageBucket: "investorportal-bad23.appspot.com",
-  messagingSenderId: "459705216111",
-  appId: "1:459705216111:web:e0bf773744b77f128cb19a",
-  measurementId: "G-KC6YTQEB1M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
 }
 
 const app = initializeApp(firebaseConfig);
